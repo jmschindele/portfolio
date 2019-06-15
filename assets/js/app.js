b1909d7ef68793a35596eaf8ad558e08b321370a
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-06-15 01:43:47 -0400

    lots of front end work. Home page transition added

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+$('#lastName').hover( function() {
+    $('#lastName').addClass('pronounce')
+  $('#lastName').text('/SHin·də·lēː/')  
+},
+function() {
+    $('#lastName').removeClass('pronounce')
+    $('#lastName').text('Schindele')
+})
+
+setTimeout(function(){
+    $('#name').removeClass('hidden')
+    $('#name').addClass('fadeIn slower')
+}, 500)
+
+setTimeout(function(){
+    $('#web-dev').removeClass('hidden')
+    $('#web-dev').addClass('fadeIn slow')
+}, 1750)
+
+
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var currentScrollTop = $(this).scrollTop();
+   if (currentScrollTop > lastScrollTop){
+       $('#title-screen').addClass('animated fadeOut')
+       $('#title-screen').on('animationend', function(){
+           $('#title-screen').addClass('hidden');
+           $('#portfolio-screen').removeClass('hidden');
+       });
+       
+   } else {
+
+   }
+   lastScrollTop = currentScrollTop;
+
+});

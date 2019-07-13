88bc75eb7139e5d0c0db29e20ec2e6aef737a4df
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-07-13 03:04:29 -0400

    styling and form updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -48,16 +48,4 @@ setTimeout(function(){
 //     span.addClass('flip-card')
 // })
 
-const coll = document.getElementsByClassName('collapsible');
-console.log(coll)
-for ( let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener('click', function() {
-        this.classList.toggle("active");
-        let content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    });
-}
+
